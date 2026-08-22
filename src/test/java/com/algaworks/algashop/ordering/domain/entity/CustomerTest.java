33e8157ddafa1ea3e7b2029a39cab6e3531eb056
Lucas Henrique <lucas.henrique0023@gmail.com>
2026-08-22 18:56:45 -0300

    Implementando a geração de UUID v7

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+class CustomerTest {
+
+    @Test
+    void testingCustomer() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "Jhon Doe",
+                LocalDate.of(1991, 7,5),
+                "jhon.doe@email.com",
+                "478-256-2504",
+                "255-08-0578",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoayltyPoints(10);
+    }
+
+}
